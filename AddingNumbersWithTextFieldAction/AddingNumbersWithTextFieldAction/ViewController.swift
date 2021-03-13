5ce6692a987e6c7f1ccb7e635d245d011249306c
Arunachalam0007 <arunsha0007@gmail.com>
2021-03-14 01:14:14 +0530

    BtnTouch with SetImage Property

--- AddingNumbersWithTextFieldAction/AddingNumbersWithTextFieldAction/ViewController.swift
+++ AddingNumbersWithTextFieldAction/AddingNumbersWithTextFieldAction/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var secondNumber: UITextField!
     @IBOutlet weak var resultBtn: UIButton!
     
+    @IBOutlet weak var resultLabel: UILabel!
     @IBAction func addNumbers(_ sender: UIButton) {
         let enteredFirstNumber:Int = Int(firstNumber.text ?? "") ?? 0
         let enteredSecondNumber:Int = Int(secondNumber.text ?? "") ?? 0
@@ -30,7 +31,9 @@ class ViewController: UIViewController {
         //print(firstNumber.text ?? 0)
 //        print(resultBtn.titleLabel?.text )
 
-        resultBtn.titleLabel?.text = String(result)
+//        resultBtn.titleLabel?.text = String(result) // NEED TO CHECK
+        
+        resultLabel.text = String(result)
 //        print(resultBtn.titleLabel?.text )
 //        print(firstNumber.text ?? 0)
         //var result = enter
@@ -38,11 +41,54 @@ class ViewController: UIViewController {
         
         resultLabel.text = String(result)
     }
-    @IBOutlet weak var resultLabel: UILabel!
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    @IBOutlet weak var firstBtn: UIButton!
+    
+    @IBAction func onTouchFirstButton(_ sender: UIButton) {
+//        sender.setImage(UIImage(named: "Google"), for: .normal)
+//        print(sender.title(for: .normal))
+//        print(sender.titleLabel?.text)
+        let btnName:String = sender.titleLabel?.text ?? "";
+        getBtnImage(onTouchBtnName:btnName , onTouchBtn: sender)
+    }
+    
+    @IBAction func onTouchSecondButton(_ sender: UIButton) {
+        let btnName:String = sender.titleLabel?.text ?? "";
+        getBtnImage(onTouchBtnName:btnName , onTouchBtn: sender)
+    }
+    
+    @IBAction func onTouchThirdButton(_ sender: UIButton) {
+        let btnName:String = sender.titleLabel?.text ?? "";
+        getBtnImage(onTouchBtnName:btnName , onTouchBtn: sender)
+    }
+    
+    @IBAction func onTouchFirthButton(_ sender: UIButton) {
+        let btnName:String = sender.titleLabel?.text ?? "";
+        getBtnImage(onTouchBtnName:btnName , onTouchBtn: sender)
+    }
+    
+    func getBtnImage(onTouchBtnName:String, onTouchBtn:UIButton)  {
+        
+        print("YOur TOuch Name",onTouchBtnName)
+        
+        switch onTouchBtnName {
+        case "onTouchFirstButton":
+            onTouchBtn.setImage(UIImage(named: "Google"), for: .normal)
+        case "onTouchSecondButton":
+            onTouchBtn.setImage(UIImage(named: "Google"), for: .normal)
+        case "onTouchThirdButton":
+            onTouchBtn.setImage(UIImage(named: "Google"), for: .normal)
+        case "onTouchFirthButton":
+            onTouchBtn.setImage(UIImage(named: "Google"), for: .normal)
+        default:
+            print("YOur Default Now")
+        }
+        
+    }
 }
 
