82e71c335355a5695d221b95410c34652ba0e481
Arunachalam0007 <arunsha0007@gmail.com>
2021-03-18 11:21:02 +0530

    Delegates

--- ObjectDelegate/ObjectDelegate/ViewController.swift
+++ ObjectDelegate/ObjectDelegate/ViewController.swift
@@ -10,21 +10,50 @@ import UIKit
 class ViewController: UIViewController,UITextFieldDelegate {
     
     let udTextField = UITextField()
+    
 
+    @IBOutlet weak var textFieldOne: UITextField!
+    
     override func viewDidLoad() {
         udTextField.frame = CGRect(x: 30, y: 30, width: 100, height: 100)
         udTextField.text = "ud Delegate"
         self.view.addSubview(udTextField)
-        // udTextField.delegate = self
+        //textFieldOne. = true
         udTextField.delegate = self
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-    // called when 'return' key pressed. return NO to ignore
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        print("textFieldShouldReturn")
-        textField.resignFirstResponder()
+        print("delegage method got called")
+        udTextField.resignFirstResponder()
+        return true
+    } // called when 'return' key pressed. return NO to ignore.
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool{
+        print("TextFiled Should Clear Called !!!")
+        textFieldOne.resignFirstResponder()
+        return true
+        
+    } // called when clear button pressed. return NO to ignore (no notifications)
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print("Text Field Called")
         return true
     }
+    
+     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        print("textFieldShouldBeginEditing Cakk")
+        return true
+    }// return NO to disallow editing.
+
+
+    
+    // called when 'return' key pressed. return NO to ignore
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        print("textFieldShouldReturn")
+//        textField.resignFirstResponder()
+//        return true
+//    }
 }
 
